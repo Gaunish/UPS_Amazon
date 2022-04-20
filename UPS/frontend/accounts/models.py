49ed90d119c2eca919e98bb79c25553ed0a059e7
gg147 <gg147@duke.edu>
2022-04-20 17:09:30 -0400

    all

--- UPS/frontend/accounts/models.py
+++ UPS/frontend/accounts/models.py
@@ -18,3 +18,38 @@ class Users(models.Model):
     class Meta:
         managed = True
         db_table = 'users'
+
+class Package(models.Model):
+    package_id = models.BigIntegerField(primary_key=True)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    truck_id = models.IntegerField()
+    user_name = models.CharField(max_length=100)
+    status = models.CharField(max_length=25)
+
+    class Meta:
+        managed = False
+        db_table = 'package'
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    package = models.ForeignKey(Package, models.DO_NOTHING)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    count = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+
+
+class Truck(models.Model):
+    truck_id = models.IntegerField(primary_key=True)
+    whid = models.IntegerField()
+    status = models.CharField(max_length=20)
+    x = models.IntegerField()
+    y = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'truck'
--- UPS/frontend/accounts/views.py
+++ UPS/frontend/accounts/views.py
@@ -5,6 +5,17 @@ from django.contrib import messages
 
 # Create your views here.
 
+#function to check whether user is logged in
+def login_required(request):
+   user = request.session.get('name', "None")
+
+   if user == "None":
+       return False
+   
+   return True
+
+
+
 
 def register_request(request):
     if request.method == "POST":
@@ -12,9 +23,26 @@ def register_request(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+           
+            request.session['name'] = form.cleaned_data['username']
+
             messages.success(request, "Registration successful.")
             return redirect("login")
         messages.error(
             request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request=request, template_name="registration/register.html", context={"register_form": form})
+
+def view_packages(request):
+    if(login_required(request) == False):
+        return
+
+    packages = Package.objects.get(user_name = request.session['name'])
+    return render(request, "home/packages.html", {"packages": packages})
+
+def package(request, id):
+    if(login_required(request) == False):
+        return
+    pack = Package.objects.get(package_id = id)
+    products = Product.objects.get(package = pack)
+    return render(request, "home/package.html", {"package": pack, "products": products})
