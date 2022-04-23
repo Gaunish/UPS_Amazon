5c8669482d31d7184b17bb2a1dd49ee9f7e14e2f
ddm99 <zcapndi@gmail.com>
2022-04-23 09:42:51 -0400

    added delivery map feature

--- UPS/frontend/frontend/views.py
+++ UPS/frontend/frontend/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from accounts.models import Package, Product
+from accounts.models import Package, Product, Truck
 from django.http import HttpResponse, JsonResponse
+from plotly.offline import plot
+import plotly.graph_objects as go
+import numpy as np
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def search(request):
@@ -29,5 +32,14 @@ def package_detail(request):
     packageid = request.GET.get("id", 0)
     if not packageid:
         return render(request, 'error.html')
+    package_result = Package.objects.get(package_id=packageid)
+    truck_result = Truck.objects.get(truck_id=package_result.truck_id)
+    plot_div = plot({"data": [go.Scatter(x=[truck_result.x], y=[truck_result.y],
+                                         mode='markers+text', text='Package', textposition='top center', name='Package', marker_color='green'), go.Scatter(x=[package_result.x], y=[package_result.y],
+                                                                                                                                                           mode='markers+text', text='Destination', name='Destination', textposition='top center', marker_color='red')],
+                     "layout": go.Layout(yaxis_range=[0, 10], xaxis_range=[0, 10],
+                                         width=600, height=600,
+                                         template="ggplot2")},
+                    output_type='div')
     products = Product.objects.filter(package_id=packageid)
-    return render(request, "package_detail.html", {'products': list(products)})
+    return render(request, "package_detail.html", {'products': list(products), 'plot_div': plot_div})
