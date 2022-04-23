052e71df0035d179c96ad81bde8ff805183dd554
ddm99 <zcapndi@gmail.com>
2022-04-23 16:45:19 -0400

    added password reset and profile

--- UPS/frontend/accounts/views.py
+++ UPS/frontend/accounts/views.py
@@ -5,9 +5,47 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Package, Product
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 # Create your views here.
 
 # function to check whether user is logged in
+
+
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "password/password_reset_email.txt"
+                    c = {
+                        "email": user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Website',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'admin@example.com',
+                                  [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return redirect("password_reset/done/")
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="password/password_reset.html", context={"password_reset_form": password_reset_form})
 
 
 def register_request(request):
@@ -49,3 +87,9 @@ def package(request, id):
         newy = form.cleaned_data['y']
         Package.objects.filter(package_id=id).update(x=newx, y=newy)
         return redirect("home")
+
+
+@login_required
+def profile(request):
+    current_user = User.objects.get(pk=request.user.id)
+    return render(request, "profile.html", {"user": current_user})
