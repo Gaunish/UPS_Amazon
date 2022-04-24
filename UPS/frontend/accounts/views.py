2e51c164a19e0c8243aab2540808bf6cba6de43d
gg147 <gg147@duke.edu>
2022-04-24 00:42:32 -0400

    email

--- UPS/frontend/accounts/views.py
+++ UPS/frontend/accounts/views.py
@@ -39,7 +39,7 @@ def password_reset_request(request):
                     }
                     email = render_to_string(email_template_name, c)
                     try:
-                        send_mail(subject, email, 'admin@example.com',
+                        send_mail(subject, email, 'ride.share.ggow@gmail.com',
                                   [user.email], fail_silently=False)
                     except BadHeaderError:
                         return HttpResponse('Invalid header found.')
